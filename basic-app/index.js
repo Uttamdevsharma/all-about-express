adccba875c0b0f807f7197206a573bf708a0d525
Uttam <uttamrohit4545@gmail.com>
2025-08-03 01:15:39 +0600

    feat: add basic routes and JSON body parsing middleware in Express server

--- basic-app/index.js
+++ basic-app/index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000
 
+
+//middleware to parse json request bodies
+app.use(express.json())
+
+//get request
 app.get('/', (req,res) => {
-    res.send("Hello World");
+    res.send("Welcome to the Server");
+})
+
+//routing with get request
+app.get("/about" , (req,res) => {
+    res.send("This is about page");
+})
+
+//post request - req.body()e oi datagula thake jegula server e pathai
+app.post("/contact" , (req,res) => {
+    console.log(req.body)
+    res.send("Message Recieved");
 })
 
 app.listen(port, () => {
