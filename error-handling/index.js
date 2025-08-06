b40bc97877e9792804f79c62e528e9718abe5a65
Uttam <uttamrohit4545@gmail.com>
2025-08-06 16:07:40 +0600

    feat: add centralized error handling middleware

---
+++ error-handling/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const errorHandler = require('./src/middleware/errorHandler')
+const port = process.env.PORT || 3000
+
+app.get('/' ,(req,res,next) => {
+    const error= new Error("Home route error")
+    next(error);
+})
+
+
+app.get('/about', (req,res,next) => {
+    fs.readFile('/abc', (err,data) => {
+        if(err){
+            next(err);
+        }         
+        res.send(data.toString())
+    })
+
+})
+
+app.get('/products', (req,res,next) => {
+    try{
+        res.send("Product page")
+    } catch(error){
+        next(error)
+    }
+})
+
+//use the middleware
+app.use(errorHandler)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
